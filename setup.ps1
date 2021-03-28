f37c2ca231a959c034ddbde0cc724ba79f913e98
abdelmohsenq <81090599+abdelmohsenq@users.noreply.github.com>
2021-03-28 13:56:33 +0200

    final changes

--- setup.ps1
+++ setup.ps1
@@ -1,13 +1,26 @@
+# Install IIS (with Management Console)
 Install-WindowsFeature -name Web-Server -IncludeManagementTools
+
+# Install ASP.NET 4.6
 Install-WindowsFeature Web-Asp-Net45
-Install-WindowsFeature Web-Asp-Net46
-Install-WindowsFeature NET-Framework-Features
+
+# ADDED:
+Install-WindowsFeature NET-Framework-Features # Not needed
+
+# ADDED: Install the .NET Core SDK
 $dotnetSdkFile = [System.IO.Path]::GetTempFileName() | Rename-Item -NewName { $_ -replace 'tmp$', 'exe' } -PassThru
 Invoke-WebRequest -Uri https://download.visualstudio.microsoft.com/download/pr/0e1818d5-9aae-49fa-8085-8e933a470a23/b95543b4ad23cbe1e6981f6efff9272c/dotnet-sdk-3.1.113-win-x64.exe -OutFile $dotnetSdkFile
 Start-Process $dotnetSdkFile -ArgumentList '/quiet' -Wait
+
+# ADDED: Install the .NET Core Windows Server Hosting bundle
 $dotnethostingFile = [System.IO.Path]::GetTempFileName() | Rename-Item -NewName { $_ -replace 'tmp$', 'exe' } -PassThru
 Invoke-WebRequest -Uri https://download.visualstudio.microsoft.com/download/pr/0f60f951-edec-48a1-aaa1-2f5b5bcbb704/e205315e03bb9f4ac0a6a7efd5d89178/dotnet-hosting-3.1.13-win.exe -OutFile $dotnethostingFile
 Start-Process $dotnethostingFile -ArgumentList '/quiet' -Wait
+
+# ADDED: Install the .NET Core runtime
+$dotnetruntimeFile = [System.IO.Path]::GetTempFileName() | Rename-Item -NewName { $_ -replace 'tmp$', 'exe' } -PassThru
+Invoke-WebRequest -Uri https://download.visualstudio.microsoft.com/download/pr/aa717f57-3ae5-48fa-a3ab-0018338d0726/fb37276b1575772461701339110e7a54/windowsdesktop-runtime-3.1.13-win-x64.exe -OutFile $dotnetruntimeFile
+Start-Process $dotnetruntimeFile -ArgumentList '/quiet' -Wait
 
 # Delete contents of wwwroot
 Remove-Item -Recurse C:\inetpub\wwwroot\*
